fe1afcaccea9d5357bd590fa1a2c8c21dcf9d6a9
emalyshev <emalyshev@wellink.ru>
2017-04-13 09:49:12 +0300

    Created content provider for tasks

--- Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -22,8 +22,10 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
-// TODO (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods
+// COMPLETED (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods
 public class TaskContentProvider extends ContentProvider {
+
+    private TaskDbHelper taskDbHelper;
 
 
     /* onCreate() is where you should initialize anything you’ll need to setup
@@ -33,10 +35,11 @@ public class TaskContentProvider extends ContentProvider {
      */
     @Override
     public boolean onCreate() {
-        // TODO (2) Complete onCreate() and initialize a TaskDbhelper on startup
+        // COMPLETED (2) Complete onCreate() and initialize a TaskDbhelper on startup
         // [Hint] Declare the DbHelper as a global variable
+        taskDbHelper = new TaskDbHelper(getContext());
 
-        return false;
+        return true;
     }
 
 
